4e2da578d18b962f4a3603fe1092ff44c7464b4c
InsightLogistics <InsightLogistics@users.noreply.github.com>
2025-10-17 06:18:24 +0000

    deploy: 6d4110ea29b824f8d0273e6788ed4ad96f5d4def

--- js/dashboard.js
+++ js/dashboard.js
@@ -303,9 +303,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!dateString) return '';
         const date = new Date(dateString);
         if (isNaN(date)) return ''; // Invalid date
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const day = date.getDate().toString().padStart(2, '0');
-        const year = date.getFullYear();
+        
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+        const day = date.getUTCDate().toString().padStart(2, '0');
+        const year = date.getUTCFullYear();
+        
         return `${month}-${day}-${year}`;
     };
 
